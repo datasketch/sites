f23920596cbe45b15296064ad24e10bd63b7bbdc
David Daza <03dazal@gmail.com>
2023-06-13 15:54:12 -0500

    initial commit

---
+++ src/components/Explorer.tsx
@@ -0,0 +1,39 @@
+import { FC, useEffect, useState } from "react";
+import { normalize } from "../utils";
+
+const Explorer: FC<{ data: Record<string, any>[] }> = ({ data }) => {
+  const [query, setQuery] = useState('')
+  const [records, setRecords] = useState(data)
+
+  useEffect(() => {
+    const result = data.filter(record => {
+      return normalize(record.title).includes(normalize(query))
+    })
+    setRecords(result)
+  }, [query])
+
+  return (
+    <>
+      <div className="max-w-screen-sm mx-auto">
+        <input className="w-full border px-2 py-1 bg-gray-100 border-transparent" type="search" value={query} onChange={e => setQuery(e.target.value)} placeholder="Search" />
+      </div>
+      <div className="mt-12">
+        {records.length ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {records.map(record => (
+              <div key={record.rcd___id} className="bg-white shadow-md px-8 py-4">
+                <img src={record.cover} className="w-32" alt="" />
+                <h3 className="font-bold">{record.title}</h3>
+                <p>{record.author}</p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p className="text-center">There are no results for query <i>&apos;{query}&apos;</i>. <button type="button" className="underline" onClick={() => setQuery('')}>Clear search</button>.</p>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default Explorer
---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Best sellers';
+export const SITE_DESCRIPTION = 'The staff of The New York Times Book Review choose the year’s standout fiction and nonfiction.';
+export const SITE_DATA = 'https://s3.amazonaws.com/uploads.dskt.ch/ddazal/best-sellers/books.json';
