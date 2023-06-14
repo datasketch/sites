6ceee95d73aa71e20d51c1cc7eeb9394a48214d8
David Daza <03dazal@gmail.com>
2023-06-14 09:52:21 -0500

    create consts from env vars

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Best sellers';
-export const SITE_DESCRIPTION = 'The staff of The New York Times Book Review choose the year’s standout fiction and nonfiction.';
-export const SITE_DATA = 'https://s3.amazonaws.com/uploads.dskt.ch/ddazal/best-sellers/books.json';
+// export const SITE_TITLE = 'Best sellers';
+// export const SITE_DESCRIPTION = 'The staff of The New York Times Book Review choose the year’s standout fiction and nonfiction.';
+// export const SITE_DATA = 'https://s3.amazonaws.com/uploads.dskt.ch/ddazal/best-sellers/books.json';
+export const SITE_TITLE = process.env.SITE_TITLE;
+export const SITE_DESCRIPTION = process.env.SITE_DESCRIPTION;
+export const SITE_DATA = process.env.SITE_DATA;
